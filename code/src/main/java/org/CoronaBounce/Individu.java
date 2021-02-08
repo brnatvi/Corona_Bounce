9595617d7222b5095c4d08f933c3134d4b0a8a38
kenza SAHI <ik_sahi@esi.dz>
2021-02-08 11:43:43 +0100

    implementation des méthodes Contaminate et Recover

--- code/src/main/java/org/CoronaBounce/Individu.java
+++ code/src/main/java/org/CoronaBounce/Individu.java
@@ -1,14 +1,9 @@
 package org.CoronaBounce;
 
-import javafx.event.ActionEvent;
-
-import java.util.Timer;
-
 public class Individu {
     public int PositionX;
     public int PositionY;
     public int Vitesse_deplacement;
-    public static int rayon_contagion;
     public String etat_sante;//je propose de mettre une enumération{ Sick,Recovered,Healthy}
     public Individu(int PosX,int PosY,int Vitesse,String etat_sante){
         this.PositionX=PosX;
@@ -19,12 +14,25 @@ public class Individu {
     public void Deplacer(int PosX,int PosY,int Vitesse){
 
     }
-    public void Recover(int duree_guerison){
-        etat_sante="Recovered";
+    public void Recover(long duree_guerison){
+        try {
+            Thread.sleep(duree_guerison);//aprés le moment de la contamination on appelle la méthode recover qui attend le temps de guerison pour que son etat de santé se modifie
+            etat_sante="Recovered";
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
 
     }
-    public void Contaminate(int duree_contamination){
-        etat_sante="Sick";
+    public void Contaminate(long duree_contamination,long duree_guerison){
+        try {
+            Thread.sleep(duree_contamination);//le thread attend une un moment pour que la contamination sera faite et aprés on appelle la méthode recover
+            etat_sante="Sick";
+            this.Recover( duree_guerison);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
     public void setEtat_sante(String etat_sante) {
         this.etat_sante = etat_sante;
@@ -37,12 +45,9 @@ public class Individu {
     public int getPositionY() {
         return PositionY;
     }
-
-    public static int getRayon_contagion() {
-        return rayon_contagion;
-    }
-
     public int getVitesse_deplacement() {
         return Vitesse_deplacement;
     }
+
+
 }
--- code/src/main/java/org/CoronaBounce/Population.java
+++ code/src/main/java/org/CoronaBounce/Population.java
@@ -7,17 +7,29 @@ public class Population {
     public ArrayList<Individu> liste_individu=new ArrayList<Individu>();
     public static double Pourcentage_guerison;
     public static double Pourcentage_contamination;
-    public static int duree_guerison;
-    public static int duree_contamination;
+    public static long duree_guerison;
+    public static long duree_contamination;
     public static int nb_Sick;//le nombre des individu contaminé
     public static int nb_Healthy;//le nombre des individus non contaminés
     public static int nb_Recovered;//le nombre des individus guéri
+    public static int rayon_contagion;
 
     public Population(){
 
     }
-    public void contamination(Individu i1,Individu i2){//i1 contamine i2
+    public double distance (Individu i1,Individu i2){
+     int x1= i1.getPositionX();
+     int x2=i2.getPositionX();
+     int y1=i1.getPositionY();
+     int y2=i2.getPositionY();
+     double dist=Math.sqrt((x1-x2)*(x1-x2)-(y1-y2)*(y1-y2));
+     return dist;
 
+    }
+    public void contamination(Individu i1,Individu i2){//i1 contamine i2
+           if(distance(i1,i2)<=rayon_contagion){
+            i2.Contaminate(duree_contamination,duree_guerison);
+           }
     }
     public double Pourcentage_contaminations(){
      int cpt=0;
