f5abbb3d15eae457998cb45e97125d7850186836
BRAGINA natalia <nata.br@gmail.com>
2021-02-11 20:52:50 +0100

    Controller added

---
+++ code/src/main/java/org/coronabounce/controllers/Controller.java
@@ -0,0 +1,53 @@
+package org.coronabounce.controllers;
+import org.coronabounce.mvcconnectors.Controllable;
+
+public class Controller implements Controllable
+{
+    @Override
+    public void setSpaceSize(int w, int h)
+    {
+
+    }
+
+    @Override
+    public void setPersonsCount(int nmbPersons)
+    {
+
+    }
+
+    @Override
+    public void setRadius(int pxls)
+    {
+
+    }
+
+    @Override
+    public int getRadius()
+    {
+        return 0;
+    }
+
+    @Override
+    public void setDurationCovid(int time)
+    {
+
+    }
+
+    @Override
+    public int getDurationCovid()
+    {
+        return 0;
+    }
+
+    @Override
+    public void setDurationNonContamination(int time)
+    {
+
+    }
+
+    @Override
+    public int getDurationNonContamination()
+    {
+        return 0;
+    }
+}
