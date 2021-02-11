e2c7df388d47cc098a06536006ff58df1c6e4a41
raveneau <emilien.raveneau@gmail.com>
2021-02-11 10:14:08 +0100

    add 1a test

---
+++ code/src/test/java/org/coronabounce/views/viewTest.java
@@ -0,0 +1,15 @@
+package org.coronabounce.view;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class viewTest extends Assertions{
+  @Test
+  public void testClose(){
+    //TODO test that close() work well.
+    //View view = new ViewGUI();
+    //view.initialize();
+    //assertTrue(view.close()); //assert that closing work without error.
+  }
+
+}
