89005107652452e5387f2df436890851912d7d7d
BRAGINA natalia <nata.br@gmail.com>
2021-02-11 21:07:39 +0100

    Some view's classes added just to make link with connectors

---
+++ code/src/main/java/org/coronabounce/views/PopulationSpace.java
@@ -0,0 +1,11 @@
+package org.coronabounce.views;
+import org.coronabounce.mvcconnectors.Controllable;
+import org.coronabounce.mvcconnectors.Displayable;
+
+public class PopulationSpace
+{
+    PopulationSpace (Displayable model, Controllable controller)
+    {
+        
+    }
+}
---
+++ code/src/main/java/org/coronabounce/views/StatisticSpace.java
@@ -0,0 +1,11 @@
+package org.coronabounce.views;
+import org.coronabounce.mvcconnectors.Controllable;
+import org.coronabounce.mvcconnectors.Displayable;
+
+public class StatisticSpace
+{
+    StatisticSpace (Displayable model, Controllable controller)
+    {
+
+    }
+}
