462d1056909d298d625860fe0d23fd40aedf35bf
Alliche zahra <allichezahra3@gmail.com>
2021-02-14 12:14:13 +0100

    Individu sera abstract

--- code/src/main/java/org/coronabounce/models/Individu.java
+++ code/src/main/java/org/coronabounce/models/Individu.java
@@ -2,12 +2,14 @@ package org.coronabounce.models;
 
 import java.util.Random;
 
-public class Individu {
+
+/** Elle sera abstract **/
+public   class  Individu {
     private String etatSante;//je propose de mettre une enumération{ Sick,Recovered,Healthy}
     private static Random r=new Random();
     public Individu(String etatSante){
-        this.etatSante=etatSante;
-    }
+      this.etatSante=etatSante;
+   }
 
 
     public void setEtatSante(String etatSante) {
@@ -16,6 +18,11 @@ public class Individu {
     public String getEtatSante(){
         return this.etatSante;
     }
+    /**********************************************
+ On mettra juste
+  Individu CreateIndividual(); //Creer un healthy, sick ou recovered person
 
-
+    public Individu actionOn(Individu x); // L'action (contaminer x ou pas)
+     +d'autres éventuelles méthodes....
+***************************************************/
 }
