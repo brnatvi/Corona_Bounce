0efaf45e1ffca18fcd8e62dc88b4a70733b1eb55
Anna Golikova <ann.golikova@gmail.com>
2021-02-22 22:08:11 +0300

    commented files

--- code/src/main/java/org/coronabounce/views/PopulationSpace.java
+++ code/src/main/java/org/coronabounce/views/PopulationSpace.java
@@ -4,22 +4,22 @@ import org.coronabounce.mvcconnectors.Displayable;
 
 public class PopulationSpace
 {
-    private Displayable model;
-    private Controllable controller;
-
-    public PopulationSpace (Displayable m, Controllable c){
-        this.model = m;
-        this.controller = c;
-    }
-
-
-    //================= Getters from Model ============================================================================/
-
-    public ArrayList<CoquilleBille> allIndividuals() { return model.getAllPoints(); }
-    public getStateIndivid() { this.allIndividuals(). }
-
-
-    //================= Own functions =================================================================================/
-
-    
+//    private Displayable model;
+//    private Controllable controller;
+//
+//    public PopulationSpace (Displayable m, Controllable c){
+//        this.model = m;
+//        this.controller = c;
+//    }
+//
+//
+//    //================= Getters from Model ============================================================================/
+//
+//    public ArrayList<CoquilleBille> allIndividuals() { return model.getAllPoints(); }
+//    public getStateIndivid() { this.allIndividuals(). }
+//
+//
+//    //================= Own functions =================================================================================/
+//
+//
 }
