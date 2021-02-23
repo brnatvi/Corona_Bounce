dd678b38bf6e374bd518e6ee0636452b6df9f384
BRAGINA natalia <nata.br@gmail.com>
2021-02-24 00:08:33 +0100

    Revert "Class Dot (abstract presentation of graphical points) created. TaskTimer..."
    This reverts commit a5188fc45a1b6db1fe1f115179fcd4d6c5f2a616

--- code/src/main/java/org/coronabounce/views/PopulationSpace.java
+++ code/src/main/java/org/coronabounce/views/PopulationSpace.java
@@ -1,6 +1,7 @@
 package org.coronabounce.views;
 
 import javafx.collections.ObservableList;
+import javafx.scene.shape.Circle;
 import org.coronabounce.models.*;
 import org.coronabounce.mvcconnectors.Controllable;
 import org.coronabounce.mvcconnectors.Displayable;
@@ -13,6 +14,11 @@ public class PopulationSpace
     private ObservableList<CoquilleBille> allPoints;
     private Timer timer;
     private TimerTask timerTask;
+
+   // class Dot extends Circle
+   // {
+   //
+   // }
 
     public PopulationSpace (Displayable m, Controllable c)
     {
@@ -30,22 +36,31 @@ public class PopulationSpace
                     String state = cb.getIndividual().healthState();
                     double coordX = cb.getPosition().getX();
                     double coordY = cb.getPosition().getY();
+                 //   if (state.equals("Healthy"))
+                 //   {
+                 //       // TODO repaint Dot
+                 //   }
+                 //   if (state.equals("Recovered"))
+                 //   {
+                 //       // TODO repaint Dot
+                 //   }
+                 //   if (state.equals("Sick"))
+                 //   {
+                 //       // TODO repaint Dot
+                 //   }
 
-                    Dot point = new Dot(coordX, coordY);
-
-                    if (state.equals("Recovered")) { point.changeColor("#ff1f40"); }
-                    if (state.equals("Sick")) { point.changeColor("DODGERBLUE"); }
+                    // TODO change position
                 }
             }
-            //TODO how to delete point on previous position ?
         };
     }
 
 
     /**
-     * This function interrogate observable list of points 30 times per second.
+     * This function interrogate observable list of points 30 times per seconds.
      */
-    public void scanState() {
+    public void scanState()
+    {
         this.timer.schedule(timerTask, 0, 33);
     }
 }
