104ebd13047a863a297019aa19eab565e856b723
BRAGINA natalia <nata.br@gmail.com>
2021-02-23 12:51:26 +0100

    corona bounce main window and settings window added

---
+++ code/src/main/java/org/coronabounce/MainController.java
@@ -0,0 +1,13 @@
+package org.coronabounce;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class MainController
+{
+
+    @FXML
+    private void switchToSettings() throws IOException {
+        App.setRoot("settings");
+    }
+}
