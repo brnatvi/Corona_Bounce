73a9136ddc699d36731f00ec235b538d3dc8a63a
kenza SAHI <ik_sahi@esi.dz>
2021-02-24 23:09:23 +0100

    implement Timers in Healthy and Recovered classes

--- code/src/main/java/org/coronabounce/models/Recovered.java
+++ code/src/main/java/org/coronabounce/models/Recovered.java
@@ -1,16 +1,23 @@
 package org.coronabounce.models;
 import java.util.Random;
-
-
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class Recovered extends Individual{
 
 
     // La personne encapsulée dans coc retrouve sa santé
-    static void recover(CoquilleBille coc , long healing_duration, long durationNonContamination )
-    {
-            coc.setIndividual(new Recovered());
+    static void recover(CoquilleBille coc , long healing_duration )
+    {     Timer t=new Timer();
+    TimerTask timerTask;
+        t.schedule(timerTask=new TimerTask() {
+            @Override
+            public void run() {
+                coc.setIndividual(new Recovered());
+            }
+        },healing_duration);
+
     }
 
 
