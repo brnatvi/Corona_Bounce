ad38628b582a95f08a8f88d9c806605f4fa84bf8
Zahra <allichezahra3@gmail.com>
2021-02-26 10:11:58 +0100

    Make sure there is not already an individual on the new generated position

--- code/src/main/java/org/coronabounce/models/Position.java
+++ code/src/main/java/org/coronabounce/models/Position.java
@@ -1,44 +1,72 @@
 package org.coronabounce.models;
 
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-public class Position {
 
+
+public class Position {
+    // Pour savoir quelles positions de la Zone sont déjà prises(Il y'a déjà un individu dessus)
+
+    private List<Position> listTakenPositions =new ArrayList<>();
     private double posX;
     private double posY;
 
-   public double getX(){ return this.posX;}
-   public double getY() { return this.posY;}
+
+    public double getX() {
+        return this.posX;
+    }
+
+    public double getY() {
+        return this.posY;
+    }
 
 
-    void setPos(double x, double y){
-        if(x >= 10 && x <= Zone.getWidth() && y >= 10 && y <=Zone.getHeight()) {
+    void setPos(double x, double y) {
+        if (x >= 10 && x <= Zone.getWidth() && y >= 10 && y <= Zone.getHeight()) {
             this.posX = x;
             this.posY = y;
         }
 
-   }
+    }
 
-   public Position ()
-   {
-    Random r= new Random();
-    this.posX=Math.abs(r.nextInt((int) Zone.getWidth()));
-    this.posY=Math.abs(r.nextInt((int) Zone.getHeight()));
+    public Position() {
 
-   }
-   public Position(double PosX,double PosY){
-       if(PosX <= Zone.getWidth() && PosY <=Zone.getHeight()) {
-           this.posX = PosX;
-           this.posY = PosY;
-       }else{
-           this.posX=0;
-           this.posY=0;
-       }
-   }
+        Random r = new Random();
+        this.posX = Math.abs(r.nextInt((int) Zone.getWidth()));
+        this.posY = Math.abs(r.nextInt((int) Zone.getHeight()));
 
 
 
+        while( !isEmpty(posX,posY))
+        {
+
+            this.posX = Math.abs(r.nextInt((int) Zone.getWidth()));
+            this.posY = Math.abs(r.nextInt((int) Zone.getHeight()));
+        }
+
+    }
+
+    public Position(double PosX, double PosY) {
+        if (PosX <= Zone.getWidth() && PosY <= Zone.getHeight()) {
+            this.posX = PosX;
+            this.posY = PosY;
+        } else {
+            this.posX = 0;
+            this.posY = 0;
+        }
+    }
+
+    public boolean isEmpty(double PosX, double PosY)
+    {
+        for (Position pos : this.listTakenPositions)
+        {
+            if(pos.posX==PosX && pos.posY==posY) return false;//Position déjà prise
+        }
+        listTakenPositions.add(new Position(PosX,PosY));
+        return true;
+    }
 
 
 }
