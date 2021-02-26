10c20904d67b3197b6cfc476a7ec13d82271c9a6
BRAGINA natalia <nata.br@gmail.com>
2021-02-26 10:54:23 +0100

    Point's movements are displayable now

--- code/src/main/java/org/coronabounce/MainController.java
+++ code/src/main/java/org/coronabounce/MainController.java
@@ -5,9 +5,13 @@ import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 import org.coronabounce.models.CoquilleBille;
 import org.coronabounce.models.Population;
 import org.coronabounce.models.Zone;
@@ -24,36 +28,32 @@ public class MainController
     @FXML
     private void initialize()
     {
-        Zone z = new Zone(400,300,20);
+        Zone z = new Zone(400,250,20);
         Population pop = z.getPopulation();
         pop.setContaminationRadius(10);
         Displayable model = z.getPopulation();
         List<CoquilleBille> allPoints = model.getAllPoints();
 
-        Timer timer = new Timer();
-        TimerTask timerTask = new TimerTask()
-        {
-            @Override
-            public void run()
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(33), ev -> {
+            panel.getChildren().retainAll();
+            for (CoquilleBille cb : allPoints)
             {
-                for (CoquilleBille cb : allPoints)
-                {
-                    String state = cb.getIndividual().healthState();
-                    double coordX = cb.getPosition().getX();
-                    double coordY = cb.getPosition().getY();
 
-                    Circle point = new Circle(coordX, coordY, 5);
-
-                    if (state.equals("Healthy")) { point.setFill(valueOf("#1abd38")); }
-                    if (state.equals("Recovered")) { point.setFill(valueOf("#ff8000")); }
-                    if (state.equals("Sick")) { point.setFill(valueOf("#14902b")); }
-
-                    panel.getChildren().add(point);
-                }
+                String state = cb.getIndividual().healthState();
+                double coordX = cb.getPosition().getX();
+                double coordY = cb.getPosition().getY();
+                Circle point = new Circle(coordX, coordY, 4);
+                if (state.equals("Healthy")) { point.setFill(valueOf("#4ACE11")); }    //green
+                if (state.equals("Recovered")) { point.setFill(valueOf("#ff8000")); }  //orange
+                if (state.equals("Sick")) { point.setFill(valueOf("#FF0040")); }      // red
+                panel.getChildren().add(point);
             }
-        };
 
-        timer.schedule(timerTask, 1, 33);
+        }));
+
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+        //timer.schedule(timerTask, 0, 33);
         z.moving();
     }
 
--- code/src/main/java/org/coronabounce/models/Zone.java
+++ code/src/main/java/org/coronabounce/models/Zone.java
@@ -39,7 +39,7 @@ public class Zone {
                 Sick.contaminate(p,5000,10000,15000);
                 p.printMovement();
             }
-        },0,1*5000);
+        },0,1*150);
 
     }
 
