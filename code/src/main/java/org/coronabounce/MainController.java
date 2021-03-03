7c68981c4d46bdc4a6cedcfda64a2c06fb00556d
BRAGINA natalia <nata.br@gmail.com>
2021-03-03 22:54:11 +0100

    Statistics and graph initialised in MainController

--- code/src/main/java/org/coronabounce/MainController.java
+++ code/src/main/java/org/coronabounce/MainController.java
@@ -7,11 +7,13 @@ import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
-import javafx.scene.chart.StackedAreaChart;
+import javafx.scene.chart.*;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 import javafx.scene.shape.Circle;
 import javafx.util.Duration;
 import org.coronabounce.controllers.Controller;
@@ -20,17 +22,17 @@ import org.coronabounce.models.Zone;
 import org.coronabounce.mvcconnectors.Controllable;
 import org.coronabounce.mvcconnectors.Displayable;
 
+import static javafx.scene.input.KeyCode.Y;
 import static javafx.scene.paint.Paint.valueOf;
 
 
 public class MainController
 {
     private Controllable controller;
-    @FXML Pane panel;
-    @FXML AnchorPane statHealthy;
-    @FXML AnchorPane statSick;
-    @FXML AnchorPane statRecovered;
-    @FXML StackedAreaChart statPanel;
+    @FXML AnchorPane mainPane;
+    @FXML Pane panel;                            // field with moving points
+    @FXML GridPane mainGrid;                     // grid contains statistic's grid (gridStatistic) and graph (graphPanel)
+    @FXML GridPane gridStatistic;
     @FXML ChoiceBox btnScenario;
     @FXML Label labelHealthy;
     @FXML Label labelSick;
@@ -48,6 +50,30 @@ public class MainController
         Displayable model = z.getPopulation();                     //TODO check it
         List<CoquilleBille> allPoints = model.getAllPoints();
 
+
+        // init graphPanel
+        NumberAxis xAxis = new NumberAxis();
+        xAxis.setTickLabelsVisible(false);
+        xAxis.setTickMarkVisible(false);
+        NumberAxis yAxis = new NumberAxis(0, model.getNbIndividus(), 1);
+        yAxis.setTickLabelsVisible(false);
+        yAxis.setTickMarkVisible(false);
+        AreaChart graphPanel = new AreaChart(xAxis, yAxis);
+        XYChart.Series healthy = new XYChart.Series();
+        XYChart.Series sick = new XYChart.Series();
+        XYChart.Series recovered = new XYChart.Series();
+        graphPanel.getData().addAll(healthy, sick, recovered);
+
+
+        // fil mainGrid by graphPanel
+        mainGrid.add(graphPanel, 1,0 );
+        graphPanel.setPrefSize(371, 160);
+        graphPanel.setLayoutX(138);
+        graphPanel.setLayoutY(59);
+        graphPanel.setHorizontalGridLinesVisible(false);
+        graphPanel.setVerticalGridLinesVisible(false);
+
+
         Timeline timeline = new Timeline(new KeyFrame(Duration.millis(33), ev -> {
             panel.getChildren().retainAll();
 
@@ -61,12 +87,17 @@ public class MainController
                 if (state.equals("Recovered")) { point.setFill(valueOf("#CF7EEE")); }  //lilas
                 if (state.equals("Sick")) { point.setFill(valueOf("#830B0B")); }      // red-brown
                 panel.getChildren().add(point);
+
+                // reload statistics
                 labelHealthy.setText(String.valueOf(model.getNbHealthy()));
                 labelSick.setText(String.valueOf(model.getNbSick()));
                 labelRecovered.setText(String.valueOf(model.getNbRecovered()));
+
+                //healthy.getData().add(new XYChart.Data("", model.getNbIndividus()));                    //TODO doesn't work
+                //sick.getData().add(new XYChart.Data("", model.getNbSick()));
+                //recovered.getData().add(new XYChart.Data("", model.getNbRecovered() + model.getNbSick()));
             }
         }));
-
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.play();
         z.moving();
