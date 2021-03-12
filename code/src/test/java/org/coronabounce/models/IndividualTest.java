c872b6349d66f30f4c7c3acddc318e279f81617c
raveneau <emilien.raveneau@gmail.com>
2021-03-12 09:26:40 +0100

    Add a test that show that individual.contact() didn't work.

--- code/src/test/java/org/coronabounce/models/IndividualTest.java
+++ code/src/test/java/org/coronabounce/models/IndividualTest.java
@@ -2,6 +2,7 @@ package org.coronabounce.models;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.coronabounce.controllers.Controller;
 
 public class IndividualTest extends Assertions{
   @Test
@@ -22,5 +23,35 @@ public class IndividualTest extends Assertions{
     i = new Healthy();
     assertTrue(!i.isSick());
   }
-
+  @Test
+  public void testContact(){
+    Controller c = new Controller();
+    Population p = new Population(c, 1, 1, 0);
+    Individual i1 = new Sick();
+    Individual i2 = new Healthy();
+    p.addIndividual(i1);
+    p.addIndividual(i2);
+    CoquilleBille coc1=null, coc2=null;
+    for (CoquilleBille coc : p.getAllPoints() ) {
+      if(coc.getIndividual().isSick()){
+        coc1 = coc;
+      }else{
+        coc2 = coc;
+      }
+    }
+    assertTrue(coc1!=null);
+    assertTrue(coc2!=null);
+    coc1.getPosition().setPos(0,0);
+    coc2.getPosition().setPos(1,1);
+    assertTrue(i1.isSick());
+    assertTrue(!i2.isSick());
+    c.setContaminationRadius(0.5);
+    p.interaction();
+    assertTrue(i1.isSick());
+    assertTrue(!i2.isSick());
+    c.setContaminationRadius(10);
+    p.interaction();
+    assertTrue(i1.isSick());
+    assertTrue(i2.isSick());
+  }
 }
