a0c7fb3009ab6362df00b11dfb8eadae9076ea4b
BRAGINA natalia <nata.br@gmail.com>
2021-03-19 12:19:23 +0100

    exit on close added

--- code/src/main/java/org/coronabounce/App.java
+++ code/src/main/java/org/coronabounce/App.java
@@ -1,15 +1,16 @@
 package org.coronabounce;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import org.coronabounce.controllers.Controller;
-import org.coronabounce.models.Population;
 import org.coronabounce.models.Zone;
 import org.coronabounce.mvcconnectors.Controllable;
-import org.coronabounce.mvcconnectors.Displayable;
 
 import java.io.IOException;
 
@@ -33,7 +34,19 @@ public class App extends Application {
         scene = new Scene(parentMain, 1000, 600);
         stage.setScene(scene);
         stage.show();
-
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                try
+                {
+                    Platform.exit();
+                    System.exit(0);
+                } catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     static void setRoot(String fxml) throws IOException {
