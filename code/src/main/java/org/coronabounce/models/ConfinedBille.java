9f12847ac40e8166b6d081b87a625193283a835a
kenza SAHI <ik_sahi@esi.dz>
2021-04-18 23:25:56 +0200

    clean and Comment Confined Bille

--- code/src/main/java/org/coronabounce/models/ConfinedBille.java
+++ code/src/main/java/org/coronabounce/models/ConfinedBille.java
@@ -14,23 +14,23 @@ public class ConfinedBille extends CoquilleBille {
         super(i);
     }
 
-    private double genererDouble() {
+    private double genererDouble() {//cette methode permet de generer un reel entre 0 et 1
         return random.nextDouble();
     }
 
-    private int genererInt(int borneInf, int borneSup) {
+    private int genererInt(int borneInf, int borneSup) {//Cette methode permet de generer un entier entre borneInf et borneSup
         int nb;
         nb = borneInf + random.nextInt(borneSup - borneInf);
         return nb;
     }
 
-    private void reduceSpeed() {
-        int percentage = genererInt(60, 100);
+    private void reduceSpeed() {// cette emthode permet de reduire la vitesse de déplacement  des coquilles d un pourcentage alétoire entre 60% et 100%
+        int percentage = genererInt(60, 100);//generer un entier entre 60 et 100
 
-        if ((this.getMovingSpeedX() * percentage / 100) <= 1 || (this.getMovingSpeedY() * percentage / 100) <= 1) {
-                this.setMovingSpeed(this.getMovingSpeedX(), this.getMovingSpeedX());
+        if ((this.getMovingSpeedX() * percentage / 100) <= 1 || (this.getMovingSpeedY() * percentage / 100) <= 1) {//verifier si la vitesse n est pas presque nulle
+                this.setMovingSpeed(this.getMovingSpeedX(), this.getMovingSpeedX());//laisser la vitesse courante de la Coquille
         } else {
-            this.setMovingSpeed((this.getMovingSpeedX() * percentage / 100), (this.getMovingSpeedY() * percentage / 100));
+            this.setMovingSpeed((this.getMovingSpeedX() * percentage / 100), (this.getMovingSpeedY() * percentage / 100));//mettre a jour la vitesse courante de la coquille
         }
 
 
@@ -39,36 +39,36 @@ public class ConfinedBille extends CoquilleBille {
     @Override
     public void move() {
 
-        reduceSpeed();
-        stayNextToHome();
-     super.move();
+        reduceSpeed();//reduire la vitesse de la Coquille
+        stayNextToHome();//verifier a cahque fois est dans sa zone de départ sinon il faut la rendre a sa zone
+        super.move();//mettre a jour la position de la Coquille
 
 
     }
-    public void stayNextToHome(){
+    public void stayNextToHome(){//cette methode permet de calculer la distance entre la position courante de la Coquille avce sa postion de départ si elle dépasse un certain kilometrage alors il faut rendre la c
         double b = genererDouble();
         double a = genererDouble() * b;
         double c;
-        if (distancePos() >= Controller.getKilometrage()) {
+        if (distancePos() >= Controller.getKilometrage()) {// dans le cas disrance(position courante ,position de depart)>Kilometrange
             if ((this.getMovingSpeedX() - a) + this.getPosition().getX() > this.getStartingPosition().getX()+Controller.getDiametreX()) {
                 c=genererDouble()*b;
 
-                this.setMovingSpeed((this.getMovingSpeedX()) - a, (this.getMovingSpeedY()+c));
+                this.setMovingSpeed((this.getMovingSpeedX()) - a, (this.getMovingSpeedY()+c));//reculer en X pour atteindre position de depart
             }
             if ((this.getMovingSpeedY() - a) + this.getPosition().getY() > this.getStartingPosition().getY()+Controller.getDiametreY()) {
 
                 c=genererDouble()*a;
-                this.setMovingSpeed((this.getMovingSpeedX()+c), (this.getMovingSpeedY() - a));
+                this.setMovingSpeed((this.getMovingSpeedX()+c), (this.getMovingSpeedY() - a));//reculer en Y pour atteindre la position de départ
             }
             if ((this.getMovingSpeedX() + a) + this.getPosition().getX() <= this.getStartingPosition().getX()+Controller.getDiametreX()) {
                 c=genererDouble()*b;
 
-                this.setMovingSpeed((this.getMovingSpeedX()) + a, (this.getMovingSpeedY()-c));
+                this.setMovingSpeed((this.getMovingSpeedX()) + a, (this.getMovingSpeedY()-c));//avancer en X pour atteindre la position de départ
             }
             if ((this.getMovingSpeedY() + a) + this.getPosition().getY() <= this.getStartingPosition().getY()+Controller.getDiametreY()) {
 
                 c=genererDouble()*a;
-                this.setMovingSpeed((this.getMovingSpeedX()-c), (this.getMovingSpeedY() + a));
+                this.setMovingSpeed((this.getMovingSpeedX()-c), (this.getMovingSpeedY() + a));//avancer en Y pour atteindre la position de départ
             }
           
         }
