7367b272574a3e710210910987d8b5f51d975cd6
BRAGINA natalia <nata.br@gmail.com>
2021-04-27 17:53:07 +0200

    new principe of ricochet added (functions ricochet() and ricochetAll())

--- code/src/main/java/org/coronabounce/models/CoquilleBille.java
+++ code/src/main/java/org/coronabounce/models/CoquilleBille.java
@@ -21,6 +21,7 @@ public class CoquilleBille {
     private static int idCpt=0;
     private Random r = new Random();
     private double minReboundSpeed=3;
+    /** Current controller contains all parameters **/
     private Controllable controller;
     private static int MAX_SPEED = 5;
 
@@ -35,9 +36,12 @@ public class CoquilleBille {
           this.currentPosition = new Position(controller,false);
           System.out.println("Position of the point have been set, but it fail to fined a free space.");
         }
-        id=idCpt++;/**increment the number of Shells that exist**/
-        this.movingSpeedX=speedX;/**update movinSpeedX**/
-        this.movingSpeedY=speedY;/** update movingSpeedY**/
+        /**increment the number of Shells that exist**/
+        id=idCpt++;
+        /**update movinSpeedX**/
+        this.movingSpeedX=speedX;
+        /** update movingSpeedY**/
+        this.movingSpeedY=speedY;
     }
 
     public CoquilleBille(Individual individual, Population pop){
@@ -51,7 +55,6 @@ public class CoquilleBille {
     public int getId(){return id;}
 
     public Position getCurrentPosition() {return this.currentPosition;}
-   
 
     public double getMovingSpeedX() { return this.movingSpeedX;}
     public double getMovingSpeedY() { return this.movingSpeedY;}
@@ -67,7 +70,7 @@ public class CoquilleBille {
     public void setIndividual(Individual individual) { this.individual=individual; }
     public Population getPopulation(){ return individual.getPopulation(); }
 
-    //==================================================== Ricochets =================================================//
+    //=====================================================================================================//
 
     public String toString(){
       return id+"pos: "+currentPosition.toString()+" speed: "+movingSpeedX+" "+movingSpeedY+"\t"+individual+"\t"+getPopulation();
@@ -84,12 +87,13 @@ public class CoquilleBille {
     *Speed will be modify if this hurt a limit of the zone.
     */
     public void move(){
+        //bounceIfHitOtherCoquilleBille();
+        this.currentPosition.setPos(this.currentPosition.getX()+this.movingSpeedX,this.currentPosition.getY()+this.movingSpeedY);
         bounceIfOutOfZone();
         bounceIfHitWall();
-        bounceIfHitOtherCoquilleBille();
-        this.currentPosition.setPos(this.currentPosition.getX()+this.movingSpeedX,this.currentPosition.getY()+this.movingSpeedY);
+        ricochetAll();
     }
-    //==================================================== Bounce if =================================================//
+    //==================================================== Bounce off walls =================================================//
     /**
      *{@summary bounce if this will go out of the zone.}<br>
      */
@@ -108,27 +112,27 @@ public class CoquilleBille {
         for (Wall wall : getPopulation().getListWall()) {
             if (wall.willCrossWallInX(this) && wall.willCrossWallInY(this)) {
                     bounce(true);
-               
               }
         }
     }
-    /**
-     *{@summary bounce if this will hit an other CoquilleBille.}<br>
-     *All CoquilleBille that this can hit are in population.
-     */
-    public void bounceIfHitOtherCoquilleBille(){
-      for(CoquilleBille coc : population.getAllPoints()){
-        if(!coc.equals(this) && getCurrentPosition().distanceFrom(coc.getCurrentPosition()) <= (2* controller.getRadiusDot())){
-          if(coc.InX(this)){
-          coc.bounce(true);
-          this.bounce(true);}
-          if(coc.InY(this)){
-            coc.bounce(false);
-            this.bounce(false);
-          }
-        }
-      }
-    }
+//    /**
+//     *{@summary bounce if this will hit an other CoquilleBille.}<br>
+//     *All CoquilleBille that this can hit are in population.
+//     */
+//   public void bounceIfHitOtherCoquilleBille(){
+//     for(CoquilleBille coc : population.getAllPoints()){
+//       if(!coc.equals(this) && getCurrentPosition().distanceFrom(coc.getCurrentPosition()) <= (2* controller.getRadiusDot())){
+//         if(coc.InX(this)){
+//         coc.bounce(true);
+//         this.bounce(true);
+//         }
+//         if(coc.InY(this)){
+//           coc.bounce(false);
+//           this.bounce(false);
+//         }
+//       }
+//     }
+//   }
 
     /**
      *{@summary bounce.}<br>
@@ -163,25 +167,25 @@ public class CoquilleBille {
         return false;
     }
 
-    private boolean isBetween(double c, double a, double b){
-        if( c<=b && c>=a ) return true;
-        return false;
-    }
+   // private boolean isBetween(double c, double a, double b){
+   //     if( c<=b && c>=a ) return true;
+   //     return false;
+   // }
 
-    public boolean InY(CoquilleBille coc){
-         if((this.getCurrentPosition().getY()-coc.getCurrentPosition().getY()<10)&& (this.getCurrentPosition().getY()-coc.getCurrentPosition().getY()>=0)||(this.getCurrentPosition().getY()-coc.getCurrentPosition().getY()<10)&& (coc.getCurrentPosition().getY()-coc.getCurrentPosition().getY()>=0) ){
-              return true;
-         }else{
-              return false;
-         }
-    }
-    public boolean InX(CoquilleBille coc){
-          if((this.getCurrentPosition().getX()-coc.getCurrentPosition().getX()<10)&& (this.getCurrentPosition().getX()-coc.getCurrentPosition().getX()>=0)||(this.getCurrentPosition().getX()-coc.getCurrentPosition().getX()<10)&& (coc.getCurrentPosition().getX()-coc.getCurrentPosition().getX()>=0) ){
-               return true;
-          }else{
-               return false;
-          }
-    }
+  //  public boolean InY(CoquilleBille coc){
+  //       if((this.getCurrentPosition().getY()-coc.getCurrentPosition().getY()<10)&& (this.getCurrentPosition().getY()-coc.getCurrentPosition().getY()>=0)||(this.getCurrentPosition().getY()-coc.getCurrentPosition().getY()<10)&& (coc.getCurrentPosition().getY()-coc.getCurrentPosition().getY()>=0) ){
+  //            return true;
+  //       }else{
+  //            return false;
+  //       }
+  //  }
+  //  public boolean InX(CoquilleBille coc){
+  //        if((this.getCurrentPosition().getX()-coc.getCurrentPosition().getX()<10)&& (this.getCurrentPosition().getX()-coc.getCurrentPosition().getX()>=0)||(this.getCurrentPosition().getX()-coc.getCurrentPosition().getX()<10)&& (coc.getCurrentPosition().getX()-coc.getCurrentPosition().getX()>=0) ){
+  //             return true;
+  //        }else{
+  //             return false;
+  //        }
+  //  }
 
     //******************************************************************************************************************************//
     
@@ -205,4 +209,58 @@ public class CoquilleBille {
         if(r.nextBoolean()){maxSpeed=maxSpeed*(-1);}
         this.movingSpeedY = r.nextDouble()*maxSpeed;
     }
+
+    //============================================= Mutual Bounces =================================================//
+
+    /**
+     *{@summary Makes ricochet if necessary with all other points.}
+     */
+    public void ricochetAll()
+    {
+        for (CoquilleBille coc : population.getAllPoints())
+        {
+            if (!this.equals(coc))
+            {
+                this.ricochet(coc, false);
+            }
+        }
+    }
+
+    /**
+     *{@summary Makes ricochet if it does not made yet.}
+     *@param coc coquilleBille to make ricochet with.
+     *@return true if ricochet was made.
+     */
+    public boolean ricochet(CoquilleBille coc, boolean isRicochet)
+    {
+        boolean isDone = false;
+        if (!isRicochet && isNear(coc))
+        {
+            double tmpX = coc.getMovingSpeedX();
+            double tmpY = coc.getMovingSpeedY();
+            coc.setMovingSpeed(this.movingSpeedX, this.movingSpeedY);
+            this.setMovingSpeed(tmpX, tmpY);
+            isDone = true;
+        }
+        return isDone;
+    }
+
+    /**
+     *{@summary Measure distance between current point and point passing as parameter.}
+     *@param coc coquilleBille to measure distance with.
+     *@return true if distance less than or equal to two radius of points (so they are contiguous).
+     */
+    public boolean isNear(CoquilleBille coc)
+    {
+        double cocX = coc.getCurrentPosition().getX();
+        double cocY = coc.getCurrentPosition().getY();
+        double currX = this.getCurrentPosition().getX();
+        double currY = this.getCurrentPosition().getY();
+        if (Math.sqrt(Math.pow(cocX - currX, 2) + Math.pow(cocY - currY, 2)) <= 2*controller.getRadiusDot())
+        {
+            return true;
+        }
+        return false;
+    }
+
 }
