00236fe7b4934e0821637511adeefba457d04bb0
kenza SAHI <ik_sahi@esi.dz>
2021-04-27 14:56:37 +0200

    Clean and Comment Incubating class

--- code/src/main/java/org/coronabounce/models/Incubating.java
+++ code/src/main/java/org/coronabounce/models/Incubating.java
@@ -8,14 +8,14 @@ import java.util.TimerTask;
 public class Incubating extends Individual{
   public Incubating(CoquilleBille coc, Population p){
     super(coc,p);
-    p.nbIncubating++;
-    p.nbHealthy--;
-    p.getTimer().schedule(new TimerTask() {
+    p.nbIncubating++;/**increase the number of incubating individuals//
+    p.nbHealthy--;/**decrease the number of healthy individuals**/
+    p.getTimer().schedule(new TimerTask() {/**plan an action that will take place after the incubation period**/
         @Override
         public void run() {
             if (Incubating.super.population.getController().getState() == Controllable.eState.Working){
-                coc.setIndividual(new Sick(coc, p));
-                p.nbIncubating--;
+                coc.setIndividual(new Sick(coc, p)); /**overwrite the Incubating  individual that exists in the shell and replace it with Sick individual**/
+                p.nbIncubating--;/**decrease the number of Incubating individuals**/
             }
         }
     }, p.getDurationIncubation());
@@ -37,7 +37,9 @@ public class Incubating extends Individual{
      */
     private void contaminate(){
         for(CoquilleBille c : population.getAllPoints()){
+             /**if the distance between two shells is less than the contamination radius then the diseased shell transmits the virus to the healthy shell**/
             if(!coc.equals(c) && population.distance(coc,c)<= population.getContaminationRadius() && c.getIndividual() instanceof Healthy){
+                 /**overwrite the healthy individual that exists in the shell and replace it with incubating individual**/
                 c.setIndividual(new Incubating(c, population));
             }
         }
