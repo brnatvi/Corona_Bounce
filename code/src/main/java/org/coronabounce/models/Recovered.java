11d14ee04b6cbaf0123995dbc32547ff1fa2e303
kenza SAHI <ik_sahi@esi.dz>
2021-04-27 15:09:34 +0200

    Clean and Comment Recovered class

--- code/src/main/java/org/coronabounce/models/Recovered.java
+++ code/src/main/java/org/coronabounce/models/Recovered.java
@@ -9,13 +9,13 @@ public class Recovered extends Individual{
 
     public Recovered(CoquilleBille coc, Population p){
       super(coc,p);
-       p.nbRecovered++;
-       p.getTimer().schedule(new TimerTask() {
+       p.nbRecovered++;/**increase the number of Recovered individuals**/
+       p.getTimer().schedule(new TimerTask() {/**plan an action that will take place after the immunity period**/
           @Override
           public void run() {
               if (p.getController().getState() == Controllable.eState.Working) {
-                  coc.setIndividual(new Healthy(coc, p));
-                  p.nbRecovered--;
+                  coc.setIndividual(new Healthy(coc, p));/**overwrite the Recovered   individual that exists in the shell and replace it with Healthy individual**/
+                  p.nbRecovered--;/**decrease the number of Recovered Individuals**/
               }
           }
        },p.getDurationImmunity());
